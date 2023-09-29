ee1e96122e259f763d930672126b6d6c0ec9c7f7
Replit user <>
2023-09-29 02:02:59 +0000

    new try

--- heap.c
+++ heap.c
@@ -18,6 +18,35 @@ typedef struct Heap{
 
 
 void* heap_top(Heap* pq){
+  Heap* createHeap() {
+    // Crear un nuevo objeto Heap y reservar memoria para él
+    Heap* heap = (Heap*) malloc(sizeof(Heap));
+    if (heap == NULL) {
+        printf("Error: no se pudo reservar memoria para el heap.\n");
+        return NULL;
+    }
+    heap->handle = HeapCreate(0, 3 * sizeof(int), 0);
+    if (heap->handle == NULL) {
+        printf("Error: no se pudo crear el heap privado.\n");
+        free(heap); 
+        return NULL;
+    }
+
+    heap->heapArray = (int*) HeapAlloc(heap->handle, 0, 3 * sizeof(int));
+    if (heap->heapArray == NULL) {
+        printf("Error: no se pudo asignar memoria para el arreglo heapArray.\n");
+        HeapDestroy(heap->handle); // Destruir el heap privado
+        free(heap); // Liberar la memoria del objeto Heap
+        return NULL;
+    }
+
+    // Inicializar las variables del objeto Heap
+    heap->capacity = 3; // La capacidad inicial es de 3 casillas
+    heap->size = 0; // El tamaño actual es de 0 casillas
+
+    // Devolver el puntero al objeto Heap creado
+    return heap;
+}
     return NULL;
 
 
