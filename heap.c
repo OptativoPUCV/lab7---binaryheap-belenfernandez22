bf4d91444264539d5440cf04b58830146713f514
Replit user <>
2023-10-14 18:26:50 +0000

    new try

--- heap.c
+++ heap.c
@@ -16,23 +16,71 @@ typedef struct Heap{
   int capac;
 } Heap;
 
-
-void* heap_top(Heap* pq){
+void* heap_top(Heap* pq)
+{
+  if(pq->size == 0)
+  {
     return NULL;
-
-  
+  }
+  return pq->heapArray->data
 }
 
-void heap_push(Heap* pq, void* data, int priority){
+/*void heap_push(Heap* pq, void* data, int p)
+{
+  if(pq->size == pq->capac)
+  {
+    pq->capac = (pq->capac * 2) + 1;
+    pq->heapArray = realloc(pq->heapArray, pq->capac * sizeof(heapElem));
+  }
 
+  int i = pq->size;
+  pq->heapArray[i].priority = p;
+  pq->heapArray[i].data = data;
+  pq->size++;
+
+  while(i > 0 && pq->heapArray[i].priority > pq->heapArray[(i - 1)/2].priority)
+  {
+    heapElem tmp = pq->heapArray[i]; //Variable temporal, para guardar dato que se desea cambiar
+    pq->heapArray[i] = pq->heapArray[(i - 1)/2];
+    pq->heapArray[(i - 1)/2] = tmp;
+    i = (i - 1)/2;
+  }
 }
 
 
-void heap_pop(Heap* pq){
+void heap_pop(Heap* pq)
+{
+  int i = pq->size;
+  pq->heapArray[0] = pq->heapArray[i-1];
+  i--;
+  int k = 0;
+  while(1)
+  {
+   int hijoD = (2 * k) + 2;
+   int hijoI = (2 * k) + 1;
+   int tmp = k;
+    if(hijoI < i && pq->heapArray[hijoI].priority > pq->heapArray[tmp].priority)
+    {
+      tmp = hijoI;
+    }
 
+    if(hijoD < i && pq->heapArray[hijoD].priority > pq->heapArray[tmp].priority)
+    {
+      tmp = hijoD;
+    }
+
+    if(tmp != k)
+    {
+      heapElem tmp2 = pq->heapArray[k];
+      pq->heapArray[k] = pq->heapArray[tmp];
+      pq->heapArray[tmp] = tmp2;
+      k = tmp;
+    }
+    break;
+  }
+  pq->size = i;
 }
 
-// Implementar la función createHeap
 Heap* createHeap() {
     // Crear un nuevo objeto Heap y reservar memoria para él
     Heap* heap = (Heap*) malloc(sizeof(Heap));
@@ -40,19 +88,27 @@ Heap* createHeap() {
         printf("Error: no se pudo reservar memoria para el heap.\n");
         return NULL;
     }
-
-    // Inicializar las variables del objeto Heap
     heap->size = 0; // El tamaño actual es de 0 casillas
     heap->capac = 3; // La capacidad inicial es de 3 casillas
-
-    // Reservar memoria para el arreglo heapArray
     heap->heapArray = (heapElem*) malloc(heap->capac * sizeof(heapElem));
     if (heap->heapArray == NULL) {
         printf("Error: no se pudo reservar memoria para el arreglo heapArray.\n");
-        free(heap); // Liberar la memoria del objeto Heap
+        free(heap); 
         return NULL;
     }
-
-    // Devolver el puntero al objeto Heap creado
     return heap;
-}
+}*/
+
+
+
+
+//
+//Heap* createHeap()
+//{
+  //Heap *arreglo = (Heap*)malloc(sizeof(Heap));
+
+  //arreglo->heapArray = (heapElem*)malloc(3 * sizeof(heapElem));
+  //arreglo->size = 0;
+  //arreglo->capac = 3;
+  //return arreglo;
+//}//
