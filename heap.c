ca3e8173536609a473cba17b7e98d506a5e8c6ce
Replit user <>
2023-10-14 18:29:43 +0000

    new try

--- heap.c
+++ heap.c
@@ -27,58 +27,13 @@ void* heap_top(Heap* pq)
 
 void heap_push(Heap* pq, void* data, int p)
 {
-  if(pq->size == pq->capac)
-  {
-    pq->capac = (pq->capac * 2) + 1;
-    pq->heapArray = realloc(pq->heapArray, pq->capac * sizeof(heapElem));
-  }
 
-  int i = pq->size;
-  pq->heapArray[i].priority = p;
-  pq->heapArray[i].data = data;
-  pq->size++;
-
-  while(i > 0 && pq->heapArray[i].priority > pq->heapArray[(i - 1)/2].priority)
-  {
-    heapElem tmp = pq->heapArray[i]; //Variable temporal, para guardar dato que se desea cambiar
-    pq->heapArray[i] = pq->heapArray[(i - 1)/2];
-    pq->heapArray[(i - 1)/2] = tmp;
-    i = (i - 1)/2;
-  }
 }
 
 
 void heap_pop(Heap* pq)
 {
-  int i = pq->size;
-  pq->heapArray[0] = pq->heapArray[i-1];
-  i--;
-  int k = 0;
-  while(1)
-  {
-   int hijoD = (2 * k) + 2;
-   int hijoI = (2 * k) + 1;
-   int tmp = k;
-    if(hijoI < i && pq->heapArray[hijoI].priority > pq->heapArray[tmp].priority)
-    {
-      tmp = hijoI;
-    }
 
-    if(hijoD < i && pq->heapArray[hijoD].priority > pq->heapArray[tmp].priority)
-    {
-      tmp = hijoD;
-    }
-
-    if(tmp != k)
-    {
-      heapElem tmp2 = pq->heapArray[k];
-      pq->heapArray[k] = pq->heapArray[tmp];
-      pq->heapArray[tmp] = tmp2;
-      k = tmp;
-    }
-    break;
-  }
-  pq->size = i;
 }
 
 Heap* createHeap() {
@@ -96,7 +51,7 @@ Heap* createHeap() {
         free(heap); 
         return NULL;
     }
-    return heap
+    return heap;
 }
 
 
